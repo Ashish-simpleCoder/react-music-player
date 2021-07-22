257c0d223ca7e2fd8f54abe057717590151f69d5
Ashish-simpleCoder <ashishprajapati357753357@gmail.com>
2021-07-22 19:26:17 +0530

    reapairs

--- src/Comps/music_data.js
+++ src/Comps/music_data.js
@@ -11,107 +11,107 @@ import sasuke_rap from './img/10_sasuke_rap.png'
 import super_hero from './img/11_super_hero.png'
 
 
-import faith_2 from './music/1_BlasterJaxx - Faith (Radio Edit).mp3'
-import bigroom_2 from './music/2_Hardwell & Blasterjaxx - Bigroom Never Dies.mp3'
-import madworld_2 from './music/3_Hardwell Ft. Jake Reese - Mad World.mp3'
-import warloards_2 from './music/4_WARLORDS Rap One-Piece(Rustage Mix).mp3'
-import jiraya_2 from './music/5_JIRAIYA Rap Sage Mode(Rustage).mp3'
-import shannin_2 from './music/6_Legendary Sannin Rap(RUSTAGE ft. Lex Bratcher).mp3'
-import onepunch_2 from "./music/7_One Punch Man  Opening The Hero.mp3"
-import i_want_2 from './music/8_The Chainsmokers & Coldplay.mp3'
-import redlight_2 from './music/9_Tiësto - Red Lights (twoloud Remix).mp3'
-import sasuke_rap_2 from './music/10_Uchiha Rap(RED)( RUSTAGE ft. Khantrast).mp3'
-import super_hero_2 from './music/11_Unknown Brain - Superhero (feat. Chris Linton).mp3'
+// import faith_2 from './music/1_BlasterJaxx - Faith (Radio Edit).mp3'
+// import bigroom_2 from './music/2_Hardwell & Blasterjaxx - Bigroom Never Dies.mp3'
+// import madworld_2 from './music/3_Hardwell Ft. Jake Reese - Mad World.mp3'
+// import warloards_2 from './music/4_WARLORDS Rap One-Piece(Rustage Mix).mp3'
+// import jiraya_2 from './music/5_JIRAIYA Rap Sage Mode(Rustage).mp3'
+// import shannin_2 from './music/6_Legendary Sannin Rap(RUSTAGE ft. Lex Bratcher).mp3'
+// import onepunch_2 from "./music/7_One Punch Man  Opening The Hero.mp3"
+// import i_want_2 from './music/8_The Chainsmokers & Coldplay.mp3'
+// import redlight_2 from './music/9_Tiësto - Red Lights (twoloud Remix).mp3'
+// import sasuke_rap_2 from './music/10_Uchiha Rap(RED)( RUSTAGE ft. Khantrast).mp3'
+// import super_hero_2 from './music/11_Unknown Brain - Superhero (feat. Chris Linton).mp3'
 
 
 
 
-const music_arr = [
-    {
-        id: 1,
-        name: 'BlasterJaxx - Faith (Radio Edit)',
-        artist: 'BlassterJaxx',
-        src: faith_2,
-        img: faith,
-    },
+// const music_arr = [
+//     {
+//         id: 1,
+//         name: 'BlasterJaxx - Faith (Radio Edit)',
+//         artist: 'BlassterJaxx',
+//         src: faith_2,
+//         img: faith,
+//     },
 
-    {
-        id: 2,
-        name: 'Bigroom Never Dies',
-        artist: 'Hardwell & Blassterjaxx',
-        src: bigroom_2,
-        img: bigroom,
-    },
+//     {
+//         id: 2,
+//         name: 'Bigroom Never Dies',
+//         artist: 'Hardwell & Blassterjaxx',
+//         src: bigroom_2,
+//         img: bigroom,
+//     },
 
-    {
-        id: 3,
-        name: 'Mad World',
-        artist: 'Hardwell ft. Jake Reese',
-        src: madworld_2,
-        img: madworld,
-    },
+//     {
+//         id: 3,
+//         name: 'Mad World',
+//         artist: 'Hardwell ft. Jake Reese',
+//         src: madworld_2,
+//         img: madworld,
+//     },
 
-    {
-        id: 4,
-        name: 'Warloards Rap(One Piece)',
-        artist: 'Rustage Mix',
-        src: warloards_2,
-        img: warloards,
-    },
+//     {
+//         id: 4,
+//         name: 'Warloards Rap(One Piece)',
+//         artist: 'Rustage Mix',
+//         src: warloards_2,
+//         img: warloards,
+//     },
 
-    {
-        id: 5,
-        name: 'Jiraya Rap Sage Mode',
-        artist: 'Rustage',
-        src: jiraya_2,
-        img: jiraya,
-    },
+//     {
+//         id: 5,
+//         name: 'Jiraya Rap Sage Mode',
+//         artist: 'Rustage',
+//         src: jiraya_2,
+//         img: jiraya,
+//     },
 
-    {
-        id: 6,
-        name: 'Legendary Shannin Rap',
-        artist: 'Rustage ft. Lex Bratcher',
-        src: shannin_2,
-        img: shannin,
-    },
+//     {
+//         id: 6,
+//         name: 'Legendary Shannin Rap',
+//         artist: 'Rustage ft. Lex Bratcher',
+//         src: shannin_2,
+//         img: shannin,
+//     },
 
-    {
-        id: 7,
-        name: 'One Punch Man',
-        artist: 'Saitama',
-        src: onepunch_2,
-        img: onepunch,
-    },
+//     {
+//         id: 7,
+//         name: 'One Punch Man',
+//         artist: 'Saitama',
+//         src: onepunch_2,
+//         img: onepunch,
+//     },
 
-    {
-        id: 8,
-        name: 'Something Just Like This',
-        artist: 'Chainsmokers & Coldplay',
-        src: i_want_2,
-        img: i_want,
-    },
+//     {
+//         id: 8,
+//         name: 'Something Just Like This',
+//         artist: 'Chainsmokers & Coldplay',
+//         src: i_want_2,
+//         img: i_want,
+//     },
 
-    {
-        id: 9,
-        name: 'Red Lights',
-        artist: 'Tiesto',
-        src: redlight_2,
-        img: redlight,
-    },
+//     {
+//         id: 9,
+//         name: 'Red Lights',
+//         artist: 'Tiesto',
+//         src: redlight_2,
+//         img: redlight,
+//     },
 
-    {
-        id: 10,
-        name: 'Uchiha Rap',
-        artist: 'Rustage ft. Kharntrast',
-        src: sasuke_rap_2,
-        img: sasuke_rap,
-    },
-    {
-        id: 11,
-        name: 'Unknown Brain - Superhero',
-        artist: 'Chris Linton(NCS Realese)',
-        src: super_hero_2,
-        img: super_hero,
-    },
-]
-export default music_arr
+//     {
+//         id: 10,
+//         name: 'Uchiha Rap',
+//         artist: 'Rustage ft. Kharntrast',
+//         src: sasuke_rap_2,
+//         img: sasuke_rap,
+//     },
+//     {
+//         id: 11,
+//         name: 'Unknown Brain - Superhero',
+//         artist: 'Chris Linton(NCS Realese)',
+//         src: super_hero_2,
+//         img: super_hero,
+//     },
+// ]
+// export default music_arr
